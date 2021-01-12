5c4d37fc364a08164c19cd7a4a91e34da2a28a3d
abonitatibus3 <abonitatibus3@gmail.com>
2021-01-11 22:31:06 -0500

    Initial commit

---
+++ src/test/java/io/github/abonitatibus3/springboottrains/SpringbootTrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abonitatibus3.springboottrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
